6d0ea0de7f3be5d579265ad8f891b6832c73f1fd
zarmir <pillihuamanhz@gmail.com>
2017-03-08 09:50:55 -0500

    FeedBack class add

---
+++ WEBAPICIp/Controllers/FeedControllerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+using WEBAPICIp.BackEnd;
+namespace WEBAPICIp.Controllers
+{
+    public class FeedControllerController : Controller
+    {
+        //
+        // GET: /FeedController/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public HttpResponseMessage Post([FromBody] Feedback Feed)
+        {HttpResponseMessage response= new HttpResponseMessage();
+            try
+            {
+            
+            return ModelState.IsValid? this.Request.CreateResponse();
+            }
+
+            catch (HttpException ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
